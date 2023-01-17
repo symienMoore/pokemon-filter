201fa5ca6ed7523d303b6582d341415aa844d693
symi <symien.moore@gmail.com>
2023-01-16 20:49:26 -0500

    .

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env sh
 
-# abort on errors
-set -e
+# # abort on errors
+# set -e
 
-# build
-yarn build
-
-# navigate into the build output directory
-cd dist
-
-# place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git checkout -B main
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:symienMoore/pokemon-filter.git main:gh-pages
-
-cd -
-
+# # build
 # yarn build
 
-# git add dist -f
-# git commit -m "Adding dist"
-# git subtree push --prefix dist origin gh-pages
+# # navigate into the build output directory
+# cd dist
+
+# # place .nojekyll to bypass Jekyll processing
+# echo > .nojekyll
+
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
+
+# git init
+# git checkout -B main
+# git add -A
+# git commit -m 'deploy'
+
+# # if you are deploying to https://<USERNAME>.github.io
+# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# # if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:symienMoore/pokemon-filter.git main:gh-pages
+
+# cd -
+
+yarn build
+
+git add dist -f
+git commit -m "Adding dist"
+git subtree push --prefix dist origin gh-pages
